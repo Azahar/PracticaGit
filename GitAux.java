e9cb4e320c7f6cbebf5ca9a21e53a1e5e1e9992e
Azahar <azahar@gmail.com>
2013-03-12 22:19:05 +0100

    Modificado por Azahar

--- GitAux.java
+++ GitAux.java
@@ -14,6 +14,9 @@ public class GitAux {
     
     public void metodoAlumno1(){
         System.out.println("metodo 1");
+
+System.out.println(" modificado por alum 1"); // insertado por alum1
+
     }
     
     public void metodoAlumno2(){
@@ -22,6 +25,9 @@ public class GitAux {
     
     public void metodoComunitario(){
         System.out.println("Aqui escribimos todos");
+        //modificacaciones alumno1 
+        System.out.println("Alumno1 inserta esta sentencia ");
+
     }
 //Añado esta linea con un comentario
 }

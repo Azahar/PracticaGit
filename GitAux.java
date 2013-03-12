7a189e36dfba2c6687dacd59f0fc2b6a9d7c3dfa
SilvioMS <silvio@uma.es>
2013-03-12 22:34:29 +0100

    conflicto resuelto

--- GitAux.java
+++ GitAux.java
@@ -9,10 +9,12 @@ package prgitejemplo;
  *
  * @author bertoa
  */
+//Añado esta linea en la rama testing
+
 public class GitAux {
     
     public void metodoAlumno1(){
-        System.out.println("metodo 1");
+        System.out.println(" modificado por alum 1"); // insertado por alum1
     }
     
     public void metodoAlumno2(){
@@ -21,9 +23,11 @@ public class GitAux {
     }
     
     public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
-        // Modicaciones SilvioMS
-        System.out.println("SilvioMS inserta esta sentencia");
+        System.out.println("alumno2 escribimos todos");
+        //modificacaciones alumno1
+        System.out.println("Alumno1 inserta esta sentencia ");
+        // Modicaciones alumno2
+        System.out.println("Alumno2  inserta esta sentencia");
 
         
     }
@@ -31,40 +35,8 @@ public class GitAux {
         // metodo rama testing
         System.out.println("Metodo de testing");
 	}
+    //Añado esta linea con un comentario
+
 }
 
-=======
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package prgitejemplo;
-
-/*
- * @author bertoa, azahar
- */
-
-//Añado esta linea en la rama testing
-
-public class GitAux {
-    
-    public void metodoAlumno1(){
-        System.out.println("metodo 1");
-
-System.out.println(" modificado por alum 1"); // insertado por alum1
-
-    }
-    
-    public void metodoAlumno2(){
-        System.out.println("metodo 2");
-    }
-    
-    public void metodoComunitario(){
-        System.out.println("Aqui escribimos todos");
-        //modificacaciones alumno1 
-        System.out.println("Alumno1 inserta esta sentencia ");
-
-    }
-//Añado esta linea con un comentario
-}
 >>>>>>> e9cb4e320c7f6cbebf5ca9a21e53a1e5e1e9992e
